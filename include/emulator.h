7ed3949f2b97b8135f0f03c4f6756f40441054c7
pandaa73 <nicholas.bamber73@gmail.com>
2025-12-27 12:37:46 +0100

    Added (untested) emulator

---
+++ include/emulator.h
@@ -0,0 +1,99 @@
+#ifndef __PANDAA73_PI_EMULATOR_H
+#define __PANDAA73_PI_EMULATOR_H
+
+#include <stdint.h>
+
+#define REGISTERS_LEN   8
+#define PORTS_LEN       8
+#define MEMORY_LEN      256
+#define CALLSTACK_LEN   32
+#define MAX_PROGRAM_LEN 2048
+
+typedef uint8_t *port_t;
+typedef unsigned short uint16_t;
+
+enum pi_flag_t {
+    PIFLG_ZERO = 0x01,
+    PIFLG_MSB  = 0x02,
+    PIFLG_BIT7 = 0x04,
+    PIFLG_LSB  = 0x08,
+    PIFLG_HLT  = 0x80,
+};
+
+enum pi_branch_condition_t {
+    PICND_BEQ  = 0b000,
+    PICND_BNE  = 0b001,
+    PICND_POS  = 0b010,
+    PICND_NEG  = 0b011,
+    PICND_PEQ  = 0b100,
+    PICND_NEQ  = 0b101,
+    PICND_EVN  = 0b110,
+    PICND_SOF  = 0b111,
+    PICND_SIZE
+};
+
+enum pi_opcode_t {
+    PIOP_NOP  = 0b00000,
+    PIOP_HLT  = 0b00001,
+    PIOP_JMP  = 0b00010,
+    PIOP_BRH  = 0b00011,
+    PIOP_CALL = 0b00100,
+    PIOP_RET  = 0b00101,
+    PIOP_LDI  = 0b00110,
+    PIOP_MOV  = 0b00111,
+    PIOP_ADD  = 0b01000,
+    PIOP_SUB  = 0b01001,
+    PIOP_ADDI = 0b01010,
+    PIOP_ADSI = 0b01011,
+    PIOP_XOR  = 0b01100,
+    PIOP_AND  = 0b01101,
+    PIOP_OR   = 0b01110,
+    PIOP_CMP  = 0b01111,
+    PIOP_XORI = 0b10000,
+    PIOP_ANDI = 0b10001,
+    PIOP_ORI  = 0b10010,
+    PIOP_CMPI = 0b10011,
+    PIOP_RSH  = 0b10100,
+    PIOP_LSH  = 0b10101,
+    PIOP_RTL  = 0b10110,
+    PIOP_ARS  = 0b10111,
+    PIOP_RSHI = 0b11000,
+    PIOP_LSHI = 0b11001,
+    PIOP_RTLI = 0b11010,
+    PIOP_ARSI = 0b11011,
+    PIOP_MST  = 0b11100,
+    PIOP_MLD  = 0b11101,
+    PIOP_PST  = 0b11110,
+    PIOP_PLD  = 0b11111,
+    PIOP_SIZE
+};
+
+struct pi_emulator_t {
+    uint8_t flags;
+    uint8_t regs[REGISTERS_LEN];
+
+    uint8_t mem[MEMORY_LEN];
+    port_t ports[PORTS_LEN];
+
+    uint16_t callstack_ptr;
+    uint16_t callstack[CALLSTACK_LEN];
+
+    uint16_t inst_ptr;
+    uint16_t program[MAX_PROGRAM_LEN];
+};
+
+void pi_emulator_init(struct pi_emulator_t *emulator);
+void pi_emulator_load_ports(
+    struct pi_emulator_t *emulator,
+    const port_t ports[PORTS_LEN]
+);
+
+void pi_emulator_load_program(
+    struct pi_emulator_t *emulator,
+    const uint16_t program[MAX_PROGRAM_LEN]
+);
+
+void pi_emulator_step(struct pi_emulator_t *emulator);
+void pi_emulator_execute(struct pi_emulator_t *emulator);
+
+#endif /* __PANDAA73_PI_EMULATOR_H */
---
+++ src/emulator.c
@@ -0,0 +1,558 @@
+#include "../include/emulator.h"
+
+#include "../include/log.h"
+
+#include <string.h>
+#include <limits.h>
+
+static inline uint8_t __rsh(uint8_t x, uint8_t n) {
+    uint8_t mask = (CHAR_BIT * sizeof(x) - 1);
+
+    n &= mask;
+
+    return x >> n;
+}
+
+static inline uint8_t __lsh(uint8_t x, uint8_t n) {
+    uint8_t mask = (CHAR_BIT * sizeof(x) - 1);
+
+    n &= mask;
+
+    return x << n;
+}
+
+static inline uint8_t __rotl(uint8_t x, uint8_t n) {
+    uint8_t mask = (CHAR_BIT * sizeof(x) - 1);
+
+    n &= mask;
+
+    return (x << n) | (x >> ((-n) & mask));
+}
+
+static inline int8_t __ars(int8_t x, uint8_t n) {
+    uint8_t mask = (CHAR_BIT * sizeof(x) - 1);
+
+    n &= mask;
+
+    return x >> n;
+}
+
+/* ========================================================================== */
+/* ============================ Branch Conditions =========================== */
+/* ========================================================================== */
+
+static inline int beq(struct pi_emulator_t *emulator);
+static inline int bne(struct pi_emulator_t *emulator);
+static inline int pos(struct pi_emulator_t *emulator);
+static inline int neg(struct pi_emulator_t *emulator);
+static inline int peq(struct pi_emulator_t *emulator);
+static inline int neq(struct pi_emulator_t *emulator);
+static inline int evn(struct pi_emulator_t *emulator);
+static inline int sof(struct pi_emulator_t *emulator);
+
+int (*check[PICND_SIZE])(struct pi_emulator_t *) = {
+    beq, bne, pos, neg, peq, neq, evn, sof
+};
+
+/* ========================================================================== */
+/* ============================== Instructions ============================== */
+/* ========================================================================== */
+
+static inline void  nop(struct pi_emulator_t *emulator);
+static inline void  hlt(struct pi_emulator_t *emulator);
+static inline void  jmp(struct pi_emulator_t *emulator);
+static inline void  brh(struct pi_emulator_t *emulator);
+static inline void call(struct pi_emulator_t *emulator);
+static inline void  ret(struct pi_emulator_t *emulator);
+static inline void  ldi(struct pi_emulator_t *emulator);
+static inline void  mov(struct pi_emulator_t *emulator);
+static inline void  add(struct pi_emulator_t *emulator);
+static inline void  sub(struct pi_emulator_t *emulator);
+static inline void addi(struct pi_emulator_t *emulator);
+static inline void adsi(struct pi_emulator_t *emulator);
+static inline void  xor(struct pi_emulator_t *emulator);
+static inline void  and(struct pi_emulator_t *emulator);
+static inline void   or(struct pi_emulator_t *emulator);
+static inline void  cmp(struct pi_emulator_t *emulator);
+static inline void xori(struct pi_emulator_t *emulator);
+static inline void andi(struct pi_emulator_t *emulator);
+static inline void  ori(struct pi_emulator_t *emulator);
+static inline void cmpi(struct pi_emulator_t *emulator);
+static inline void  rsh(struct pi_emulator_t *emulator);
+static inline void  lsh(struct pi_emulator_t *emulator);
+static inline void  rtl(struct pi_emulator_t *emulator);
+static inline void  ars(struct pi_emulator_t *emulator);
+static inline void rshi(struct pi_emulator_t *emulator);
+static inline void lshi(struct pi_emulator_t *emulator);
+static inline void rtli(struct pi_emulator_t *emulator);
+static inline void arsi(struct pi_emulator_t *emulator);
+static inline void  mst(struct pi_emulator_t *emulator);
+static inline void  mld(struct pi_emulator_t *emulator);
+static inline void  pst(struct pi_emulator_t *emulator);
+static inline void  pld(struct pi_emulator_t *emulator);
+
+void (*execute[PIOP_SIZE])(struct pi_emulator_t *) = {
+     nop,  hlt,  jmp,  brh, call,  ret,  ldi,  mov,
+     add,  sub, addi, adsi,  xor,  and,   or,  cmp,
+    xori, andi,  ori, cmpi,  rsh,  lsh,  rtl,  ars,
+    rshi, lshi, rtli, arsi,  mst,  mld,  pst,  pld,
+};
+
+/* ========================================================================== */
+/* =========================== Emulator Functions =========================== */
+/* ========================================================================== */
+
+void pi_emulator_init(struct pi_emulator_t *emulator) {
+    if(!emulator) { PLG_FATAL("init: emulator is NULL"); }
+
+    memset(emulator, 0x00, sizeof(*emulator));
+}
+
+void pi_emulator_load_ports(
+    struct pi_emulator_t *emulator,
+    const port_t ports[PORTS_LEN]
+) {
+    if(!emulator) { PLG_FATAL("load_ports: emulator is NULL"); }
+    if(!ports)    { PLG_FATAL("load_ports: ports is NULL"); }
+
+    for(uint16_t i = 0; i < PORTS_LEN; ++i) {
+        emulator->ports[i] = ports[i];
+    }
+}
+
+void pi_emulator_load_program(
+    struct pi_emulator_t *emulator,
+    const uint16_t program[MAX_PROGRAM_LEN]
+) {
+    if(!emulator) { PLG_FATAL("load_program: emulator is NULL"); }
+    if(!program)  { PLG_FATAL("load_program: program is NULL"); }
+
+    for(uint16_t i = 0; i < MAX_PROGRAM_LEN; ++i) {
+        emulator->program[i] = program[i];
+    }
+}
+
+void __unsafe_pi_emulator_step(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    /*
+     * Note that the opcode must be valid (i.e. opcode < PI_SIZE) since it's a
+     * 5-bit number
+     */
+    const enum pi_opcode_t opcode = instruction >> 11;
+
+    execute[opcode](emulator);
+
+    emulator->inst_ptr = (emulator->inst_ptr + 1) % MAX_PROGRAM_LEN;
+}
+
+void pi_emulator_step(struct pi_emulator_t *emulator) {
+    if(!emulator) { PLG_FATAL("step: emulator is NULL"); }
+
+    __unsafe_pi_emulator_step(emulator);
+}
+
+void pi_emulator_execute(struct pi_emulator_t *emulator) {
+    if(!emulator) { PLG_FATAL("execute: emulator is NULL"); }
+
+    while((emulator->flags & PIFLG_HLT) == 0) {
+        __unsafe_pi_emulator_step(emulator);
+    }
+}
+
+/* ========================================================================== */
+/* ============================ Branch Conditions =========================== */
+/* ========================================================================== */
+
+static inline int beq(struct pi_emulator_t *emulator) {
+    return (emulator->flags & PIFLG_ZERO) != 0;
+}
+
+static inline int bne(struct pi_emulator_t *emulator) {
+    return (emulator->flags & PIFLG_ZERO) == 0;
+}
+
+static inline int pos(struct pi_emulator_t *emulator) {
+    return (emulator->flags & (PIFLG_ZERO | PIFLG_MSB)) == 0;
+}
+
+static inline int neg(struct pi_emulator_t *emulator) {
+    return (emulator->flags & PIFLG_MSB) != 0;
+}
+
+static inline int peq(struct pi_emulator_t *emulator) {
+    return (emulator->flags & PIFLG_MSB) == 0;
+}
+
+static inline int neq(struct pi_emulator_t *emulator) {
+    return (emulator->flags & (PIFLG_ZERO | PIFLG_MSB)) != 0;
+}
+
+static inline int evn(struct pi_emulator_t *emulator) {
+    return (emulator->flags & PIFLG_LSB) == 0;
+}
+
+static inline int sof(struct pi_emulator_t *emulator) {
+    return ((emulator->flags & PIFLG_MSB) != 0)
+        ^ ((emulator->flags & PIFLG_BIT7));
+}
+
+/* ========================================================================== */
+/* ============================== Instructions ============================== */
+/* ========================================================================== */
+
+static inline void  nop(struct pi_emulator_t *emulator) {
+    return;
+}
+
+static inline void  hlt(struct pi_emulator_t *emulator) {
+    emulator->flags |= PIFLG_HLT;
+}
+
+static inline void  jmp(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    emulator->inst_ptr = instruction & 0x07FF;
+}
+
+static inline void  brh(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const enum pi_branch_condition_t cond = (instruction >> 8) & 0x07;
+    const uint8_t address = instruction & 0xFF;
+
+    if(!check[cond](emulator)) return;
+
+    emulator->inst_ptr = (emulator->inst_ptr & 0x700) | address;
+}
+
+static inline void call(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    emulator->callstack[emulator->callstack_ptr] = emulator->inst_ptr;
+
+    emulator->callstack_ptr = (emulator->callstack_ptr + 1) % CALLSTACK_LEN;
+
+    emulator->inst_ptr = instruction & 0x07FF;
+}
+
+static inline void  ret(struct pi_emulator_t *emulator) {
+    emulator->inst_ptr = emulator->callstack[emulator->callstack_ptr];
+
+    emulator->callstack_ptr =
+        (emulator->callstack_ptr + CALLSTACK_LEN - 1) % CALLSTACK_LEN;
+}
+
+static inline void  ldi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A   = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0xFF;
+
+    if(A != 0) { emulator->regs[A] = imm; }
+}
+
+static inline void  mov(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(A != 0) { emulator->regs[A] = emulator->regs[B]; }
+}
+
+static inline void  add(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) { emulator->regs[C] = emulator->regs[A] + emulator->regs[B]; }
+}
+
+static inline void  sub(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) { emulator->regs[C] = emulator->regs[A] - emulator->regs[B]; }
+}
+
+static inline void addi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A   = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0xFF;
+
+    if(A != 0) { emulator->regs[A] += imm; }
+}
+
+static inline void adsi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    uint8_t sximm   = (instruction >> 0) & 0x1F;
+
+    sximm |= (sximm & 0x10) << 1;
+    sximm |= (sximm & 0x10) << 2;
+    sximm |= (sximm & 0x10) << 3;
+
+    if(A != 0) { emulator->regs[C] = emulator->regs[A] + sximm; }
+}
+
+static inline void  xor(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) { emulator->regs[C] = emulator->regs[A] ^ emulator->regs[B]; }
+}
+
+static inline void  and(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) { emulator->regs[C] = emulator->regs[A] & emulator->regs[B]; }
+}
+
+static inline void   or(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) { emulator->regs[C] = emulator->regs[A] | emulator->regs[B]; }
+}
+
+static inline void  cmp(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    uint8_t diff = emulator->regs[A] - emulator->regs[B];
+
+    emulator->flags = 0; /* CPU shouldn't be halted at this stage */
+    if(         diff == 0) { emulator->flags |= PIFLG_ZERO; }
+    if((diff & 0x80) != 0) { emulator->flags |= PIFLG_MSB; }
+    if((diff & 0x01) != 0) { emulator->flags |= PIFLG_LSB; }
+    if((diff & 0x40) != 0) { emulator->flags |= PIFLG_BIT7; }
+}
+
+static inline void xori(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A   = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0xFF;
+
+    if(A != 0) { emulator->regs[A] ^= imm; }
+}
+
+static inline void andi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A   = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0xFF;
+
+    if(A != 0) { emulator->regs[A] &= imm; }
+}
+
+static inline void  ori(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A   = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0xFF;
+
+    if(A != 0) { emulator->regs[A] |= imm; }
+}
+
+static inline void cmpi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A   = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0xFF;
+
+    uint8_t diff = emulator->regs[A] - imm;
+
+    emulator->flags = 0; /* CPU shouldn't be halted at this stage */
+    if(         diff == 0) { emulator->flags |= PIFLG_ZERO; }
+    if((diff & 0x80) != 0) { emulator->flags |= PIFLG_MSB; }
+    if((diff & 0x40) != 0) { emulator->flags |= PIFLG_BIT7; }
+    if((diff & 0x01) != 0) { emulator->flags |= PIFLG_LSB; }
+}
+
+static inline void  rsh(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) {
+        emulator->regs[C] = __rsh(
+            emulator->regs[A],
+            -emulator->regs[B]
+        );
+    }
+}
+
+static inline void  lsh(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) {
+        emulator->regs[C] = __lsh(
+            emulator->regs[A],
+            emulator->regs[B]
+        );
+    }
+}
+
+static inline void  rtl(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    if(C != 0) {
+        emulator->regs[C] = __rotl(
+            emulator->regs[A],
+            emulator->regs[B]
+        );
+    }
+}
+
+static inline void  ars(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t C = (instruction >> 5) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    /*
+     * Immediate might have to be negative although it is not marked as such
+     * in the ISA. I am not sure.
+     */
+
+    if(C != 0) {
+        emulator->regs[C] = __ars(
+            emulator->regs[A],
+            emulator->regs[B]
+        );
+    }
+}
+
+static void rshi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0x07;
+
+    if(A != 0) {
+        emulator->regs[A] = __rsh(
+            emulator->regs[A],
+            -imm
+        );
+    }
+}
+
+static void lshi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0x07;
+
+    if(A != 0) {
+        emulator->regs[A] = __lsh(
+            emulator->regs[A],
+            imm
+        );
+    }
+}
+
+static void rtli(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0x07;
+
+    if(A != 0) {
+        emulator->regs[A] = __rotl(
+            emulator->regs[A],
+            imm
+        );
+    }
+}
+
+static void arsi(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint8_t imm = (instruction >> 0) & 0x07;
+
+    /*
+     * Immediate might have to be negative although it is not marked as such
+     * in the ISA. I am not sure.
+     */
+
+    if(A != 0) {
+        emulator->regs[A] = __ars(
+            emulator->regs[A],
+            imm
+        );
+    }
+}
+
+static void  mst(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    emulator->mem[emulator->regs[B]] = emulator->regs[A];
+}
+
+static void  mld(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A = (instruction >> 8) & 0x07;
+    const uint16_t B = (instruction >> 0) & 0x07;
+
+    emulator->regs[A] = emulator->mem[emulator->regs[B]];
+}
+
+static void  pst(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A    = (instruction >> 8) & 0x07;
+    const uint16_t port = (instruction >> 0) & 0x07;
+
+    if(emulator->ports[port] != NULL) {
+        *emulator->ports[port] = emulator->regs[A];
+    }
+}
+
+static void  pld(struct pi_emulator_t *emulator) {
+    const uint16_t instruction = emulator->program[emulator->inst_ptr];
+
+    const uint16_t A    = (instruction >> 8) & 0x07;
+    const uint16_t port = (instruction >> 0) & 0x07;
+
+    if(emulator->ports[port] != NULL) {
+        emulator->regs[A] = *emulator->ports[port];
+    }
+}
